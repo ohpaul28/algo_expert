f8128fea2c6bd70433fff27b1acb41ed70f5bf0f
Paul <dustin.paul.oh@gmail.com>
2022-05-17 14:31:06 -0700

    tournamentWinner solved

---
+++ dsa/javascript.js
@@ -0,0 +1,44 @@
+function tournamentWinner(competitions, results) {
+  // Write your code here.
+	let hash = {}
+	for (let i = 0; i < competitions.length; i++) {
+		let competition = competitions[i];
+		let result = results[i];
+
+		if (result === 0) {
+		let winner = competition[1]
+		let loser = competition[0]
+
+			if (!hash[winner]) {
+				hash[winner] = 3
+			} else hash[winner] += 3
+
+			if (!hash[loser]) {
+				hash[loser] = 0
+			}
+		}
+
+		if (result === 1) {
+		let winner = competition[0]
+		let loser = competition[1]
+
+			if (!hash[winner]) {
+				hash[winner] = 3
+			} else hash[winner] += 3
+
+			if (!hash[loser]) {
+				hash[loser] = 0
+			}
+		}
+	}
+	let max = '';
+	let maxValue = 0;
+	for (let key in hash) {
+		if (hash[key] > maxValue) {
+			max = key
+			maxValue = hash[key]
+		}
+	}
+
+  return max;
+}
